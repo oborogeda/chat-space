f14d6e2a27168a2601d3c157609d450e29418cb1
oborogeda <56952154+oborogeda@users.noreply.github.com>
2019-11-12 13:21:08 +0900

    update2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -64,7 +64,7 @@ $('#new_message').on('submit', function(e){
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast'); 
     })
     .fail(function() {
-      console.log('error');
+      alert('自動更新に失敗しました');
     });
   }
 };
